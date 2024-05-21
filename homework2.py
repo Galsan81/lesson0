9d0858c2980399e11d67ed92b5e954bb847d2db6
Galsan81 <galsmizhit@gmail.com>
2024-05-21 08:17:09 +0800

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kolichistvo_dz=12
+kolichistvo_chas=1.5
+nazv_kursov='Python'
+vremy_na_zad=kolichistvo_chas/kolichistvo_dz
+print(nazv_kursov+',', 'всего задач'+':', str(kolichistvo_dz)+',', 'затрачено часов'+':',str(kolichistvo_chas)+',', 'среднее выполнение', vremy_na_zad, 'часов')
