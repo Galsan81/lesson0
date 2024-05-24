9a0372ded5c348e80fa4227102540cffa8c93520
Galsan81 <galsmizhit@gmail.com>
2024-05-24 15:14:09 +0800

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,12 @@
+my_list=['banans', 'grushi', 'yabloks', 'tikvi', 12, 'fignya']
+print('list:',my_list)
+print('First el:', my_list[0],'Last el:', my_list[-1])
+print('Sublist:', my_list[2:5])
+my_list[2]='zamena'
+print('Modified list:',my_list)
+my_dict={'vegetabke': 'ovoshi', 'book': 'kniga', 'pen': 'karandash'}
+print('Dictionary:',my_dict)
+my_dict['paper']='bumaga'
+print('Translate:', my_dict['paper'])
+my_dict.update({'no':'net', 'book':'vashe'})
+print('Modified dictionary:',my_dict)
